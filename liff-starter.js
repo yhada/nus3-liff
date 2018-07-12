ac83e707a9c1c4bdc2988565fdf27a204e7e3c21
yhada <yhada@techloco.co.jp>
2018-07-12 15:22:55 +0900

    fix: return to original.

--- liff-starter.js
+++ liff-starter.js
@@ -1,21 +1,31 @@
 window.onload = function (e) {
-    liff.init((data) => {
+    liff.init(function (data) {
         initializeApp(data);
     });
-}
+};
 
-const initializeApp = (data) => {
-    const open = () => {
+function initializeApp(data) {
+    document.getElementById('languagefield').textContent = data.language;
+    document.getElementById('viewtypefield').textContent = data.context.viewType;
+    document.getElementById('useridfield').textContent = data.context.userId;
+    document.getElementById('utouidfield').textContent = data.context.utouId;
+    document.getElementById('roomidfield').textContent = data.context.roomId;
+    document.getElementById('groupidfield').textContent = data.context.groupId;
+
+    // openWindow call
+    document.getElementById('openwindowbutton').addEventListener('click', function () {
         liff.openWindow({
             url: 'https://line.me'
         });
-    }
+    });
 
-    const close = () => {
+    // closeWindow call
+    document.getElementById('closewindowbutton').addEventListener('click', function () {
         liff.closeWindow();
-    }
+    });
 
-    const sendMessage = () => {
+    // sendMessages call
+    document.getElementById('sendmessagebutton').addEventListener('click', function () {
         liff.sendMessages([{
             type: 'text',
             text: "You've successfully sent a message! Hooray!"
@@ -28,20 +38,36 @@ const initializeApp = (data) => {
         }).catch(function (error) {
             window.alert("Error sending message: " + error);
         });
-    }
+    });
 
-    const loadProfile = () => {
+    //get profile call
+    document.getElementById('getprofilebutton').addEventListener('click', function () {
         liff.getProfile().then(function (profile) {
-            const message = 
-                `
-                    profile.userId:         ${profile.userId}
-                    profile.displayName:    ${profile.displayName}
-                    profile.pictureUrl:     ${profile.pictureUrl}
-                    profile.statusMessage:  ${profile.statusMessage}
-                `;
-            window.alert(message);
+            document.getElementById('useridprofilefield').textContent = profile.userId;
+            document.getElementById('displaynamefield').textContent = profile.displayName;
+
+            var profilePictureDiv = document.getElementById('profilepicturediv');
+            if (profilePictureDiv.firstElementChild) {
+                profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
+            }
+            var img = document.createElement('img');
+            img.src = profile.pictureUrl;
+            img.alt = "Profile Picture";
+            profilePictureDiv.appendChild(img);
+
+            document.getElementById('statusmessagefield').textContent = profile.statusMessage;
+            toggleProfileData();
         }).catch(function (error) {
             window.alert("Error getting profile: " + error);
         });
+    });
+}
+
+function toggleProfileData() {
+    var elem = document.getElementById('profileinfo');
+    if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
+        elem.style.display = "none";
+    } else {
+        elem.style.display = "block";
     }
 }
